94ab17623082d9491692e21534a6682f4f59b496
Ry268 <ryotaro545@gmail.com>
2022-10-19 21:43:56 +0900

    add models

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
-from flask import Flask, render_template
+import os
 
+from flask import Flask, render_template, redirect, request, flash, session, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required
+from werkzeug.security import check_password_hash, generate_password_hash
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.urandom(24)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///passmane.db"
+db = SQLAlchemy(app)
+
+
+
+
+
+
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
